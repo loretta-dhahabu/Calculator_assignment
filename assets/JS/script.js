8c7c213e2b4fa7561309a8d792da29503ac57de8
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-25 17:00:55 +0300

    add style

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,22 +1,22 @@
 //here goes my code to add functionality to the html page
-let display = document.querySelector("#display");
-let clearNum = document.querySelector("#clearNumbers");
+let displayNum = document.querySelector("#display-number");
+let clearNumber = document.querySelector("#clearNumbers");
 
-function calcNumbers(x) {
-  display.value = display.value + x;
+function calcNumbers(i) {
+  display.value = display.value + i;
 }
 
-//DELETE AND CLEAR
+//Clear and Delete
 clearNum.addEventListener("click", () => {
   if ("c") {
-    display.value = "";
+    displayNum.value = "";
   }
 });
 
-function deleteNum() {
+function deleteNumber() {
   if ("del") {
-    let arr = display.value.split("");
-    arr.pop();
-    display.value = arr.join("");
+    let array = displayNum.value.split("");
+    array.pop();
+    displayNum.value = array.join("");
   }
 }
